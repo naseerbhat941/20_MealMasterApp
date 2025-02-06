a1b9b8a21b7e81b24c2e2a2ae14aed92c5e42388
Divyam Chitransh <divyam.20scse1010914@galgotiasuniversity.edu.in>
2025-02-06 21:43:08 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,9 @@ import express from "express";
 import { connectToDb } from "./db.js";
 import authRoutes from './routes/authRoutes.js'
 import adminRoutes from "./routes/adminRoutes.js"
+import ingredientsroutes from './routes/ingredientsroutes.js';
+import nutrionalroutes from './routes/nutritionaltrackingroutes.js';
+import reciperoutes from './routes/reciperoutes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
@@ -10,6 +13,9 @@ const dbURI = process.env.dbURI;
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/admin", adminRoutes)
+app.use("/api/recipe",reciperoutes);
+app.use("api/daily-track",nutrionalroutes);
+app.use("api/ingredients",ingredientsroutes);
 
 const start = async () => {
   try {
