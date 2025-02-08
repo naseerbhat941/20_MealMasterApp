aec0d337e581843191b8cdfde54cf4066180900f
divyam.20scse1010914@galgotiasuniversity.edu.in <divyam.20scse1010914@galgotiasuniversity.edu.in>
2025-02-08 23:10:20 +0530

    mealplan Controller

---
+++ Backend/controllers/mealPlanController.js
@@ -0,0 +1,48 @@
+const MealPlan = require('../models/MealPlan.js');
+const Recipe = require('../models/recipemodel.js');
+
+const createPlan = async(req,res) => {
+    try{
+        const {user,weekStartDate, meals} = req.body;
+        let mealPlan = await MealPlan.findOne({user,weekStartDate});
+        if(mealPlan){
+            mealPlan.meals = meals;
+            await mealPlan.save();
+        }else{
+            mealPlan = new MealPlan({user,weekStartDate,meals});
+            await mealPlan.save();
+        }
+        res.status(201).json({message: 'Meal Plan Created!',mealPlan});
+    }
+    catch(error){
+        res.status(500).json({error: 'unable to save meal plan!'});
+    }
+};
+
+const getPlan = async(req,res) => {
+    try{
+        const userId = req.params.userId;
+        const mealPlan = await MealPlan.findOne({user: userId}).sort({weekStartDate: -1}).populate('meals.recipeId');
+        if(!mealPlan){
+            return res.status(404).json({message: 'No Mealplan for the user of this week!'});
+        }
+        return res.status(200).json({mealPlan});
+    }
+    catch(error){
+        res.status(500).json({error: 'Unable to fetch mealplan!'});
+    }
+};
+
+const suggestPlans = async(req,res) => {
+    try{
+        const {dietaryPreferences} = req.body;
+        const suggestRecipe = await Recipe.find({category: {$in: dietaryPreferences}}).limit(10);
+        res.status(200).json({suggestRecipe});
+    }
+    catch(error){
+        res.status(500).json({error: 'Unable to get recipes!'});
+    }
+}
+
+
+module.exports = {createPlan,getPlan,suggestPlans};
