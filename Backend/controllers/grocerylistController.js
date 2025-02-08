41514faf698fca275d81ff6978f117c106ff8d46
divyam.20scse1010914@galgotiasuniversity.edu.in <divyam.20scse1010914@galgotiasuniversity.edu.in>
2025-02-09 00:40:25 +0530

    Grocery List controller

---
+++ Backend/controllers/grocerylistController.js
@@ -0,0 +1,27 @@
+const MealPlan = require('../models/MealPlan.js');
+const Recipe = require('../models/recipemodel.js');
+
+const groceryList = async(req,res) => {
+    try{
+        const userId = req.params.userId;
+        const mealPlan = await MealPlan.findOne({user: userId}).populate('meals.recipeId');
+        if(!mealPlan){
+            return res.status(404).json({message: 'No Meal plan Found!'});
+        }
+        let Grocery_List = {};
+        mealPlan.meals.forEach(meal => {
+            const recipe = meal.recipeId;
+            if(recipe && recipe.ingredients){
+                recipe.ingredients.forEach(ingredient => {
+                    Grocery_List[ingredient] = (Grocery_List[ingredient] || 0) + 1;
+                });
+            }
+        });
+        return res.status(200).json({Grocery_List});
+    }
+    catch(error){
+        res.status(500).json({error: 'Unable to create grocery list'});
+    }
+}
+
+module.exports = {groceryList};
