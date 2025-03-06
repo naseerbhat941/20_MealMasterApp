48cbd55ab52f6e94b3e4a4d1420efc2facc3a2a0
naseerbhat941 <naseerbhat941@gmail.com>
2025-03-06 14:29:36 +0530

    debuged

--- FrontEnd/MealMaster/src/Components/ButtonComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ButtonComponent({ onClick, children }) {
-  return (
-    <button onClick={onClick} className="w-full p-2 bg-blue-500 text-white rounded-lg mt-4">
-      {children}
-    </button>
-  );
-}
